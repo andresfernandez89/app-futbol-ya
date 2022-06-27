736edbb6604318c200e9b66eae0c1e1ec591ee1d
Andres Alejandro Fernandez <andresfernandez.mdp@gmail.com>
2022-06-27 00:17:25 -0300

    navbar mockData

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,29 +1,30 @@
 import Navbar from "../navbar/Navbar";
-import { Layout, Typography, Row, Col } from "antd";
+import {Layout, Typography, Row, Col} from "antd";
 import mainLogo from "../../assets/icons/main-logo.png";
 import styles from "./App.module.scss";
-const { Content } = Layout;
-const { Title } = Typography;
+import data from "../../db/mockData.json";
+const {Content} = Layout;
+const {Title} = Typography;
 
 const App = () => {
-  return (
-    <div className={styles.mainContainer}>
-      <Navbar />
-      <Content className={styles.content}>
-        <Row>
-          <Col md={9} offset={1}>
-            <img className={styles.logo} src={mainLogo} alt="logo principal" />
-          </Col>
-          <Col flex="auto">
-            <Title level={1} italic>
-              Primera Division Argentina
-            </Title>
-          </Col>
-        </Row>
-      </Content>
-      <footer className={styles.footer}>© 2022 Copyright - futbolya.com</footer>
-    </div>
-  );
+	return (
+		<div className={styles.mainContainer}>
+			<Navbar data={data} />
+			<Content className={styles.content}>
+				<Row>
+					<Col md={9} offset={1}>
+						<img className={styles.logo} src={mainLogo} alt="logo principal" />
+					</Col>
+					<Col flex="auto">
+						<Title level={1} italic>
+							{data.country[0].leagues[0].name}
+						</Title>
+					</Col>
+				</Row>
+			</Content>
+			<footer className={styles.footer}>© 2022 Copyright - futbolya.com</footer>
+		</div>
+	);
 };
 
 export default App;
