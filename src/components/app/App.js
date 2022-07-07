fb639f86e6d0c2b4c119c8a7dde5dee4af84c2f4
Andres Alejandro Fernandez <andresfernandez.mdp@gmail.com>
2022-07-07 20:37:07 -0300

    change main bg

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,16 @@
 import Navbar from "../navbar/Navbar";
-import {Layout, Typography, Row, Col} from "antd";
+import {Layout} from "antd";
 import mainLogo from "../../assets/icons/main-logo.png";
 import styles from "./App.module.scss";
 import data from "../../db/mockData.json";
 const {Content} = Layout;
-const {Title} = Typography;
 
 const App = () => {
 	return (
 		<div className={styles.mainContainer}>
 			<Navbar data={data} />
 			<Content className={styles.content}>
-				<Row>
-					<Col md={9} offset={1}>
-						<img className={styles.logo} src={mainLogo} alt="logo principal" />
-					</Col>
-					<Col flex="auto">
-						<Title level={1} italic>
-							{data.country[0].leagues[0].name}
-						</Title>
-					</Col>
-				</Row>
+				<img className={styles.logo} src={mainLogo} alt="logo principal" />
 			</Content>
 			<footer className={styles.footer}>© 2022 Copyright - futbolya.com</footer>
 		</div>
