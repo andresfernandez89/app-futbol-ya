f37f3bc885bb5586865b288b1219b21aedd5efc5
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2022-08-19 15:07:41 -0300

    Add more margin top and bottom on containerMain

--- src/components/detailContainer/DetailContainer.js
+++ src/components/detailContainer/DetailContainer.js
@@ -1,17 +1,20 @@
 import Fixture from "../fixture/Fixture";
 import Standings from "../standings/Standings";
 import {Row, Col} from "antd";
+import styles from "./DetailContainer.module.scss";
 
 const DetailContainer = () => {
 	return (
-		<Row justify="space-evenly">
-			<Col xs={22} sm={22} md={22} lg={10}>
-				<Standings />
-			</Col>
-			<Col xs={22} sm={22} md={22} lg={10}>
-				<Fixture />
-			</Col>
-		</Row>
+		<div className={styles.mainContainer}>
+			<Row justify="space-evenly">
+				<Col xs={22} sm={22} md={22} lg={10}>
+					<Standings />
+				</Col>
+				<Col xs={22} sm={22} md={22} lg={10}>
+					<Fixture />
+				</Col>
+			</Row>
+		</div>
 	);
 };
 
