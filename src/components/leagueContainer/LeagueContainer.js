c2730c2f45ec09a3fbea58147579e72dbedf90f5
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2022-09-13 18:16:41 -0300

    leagueContainer component added

---
+++ src/components/leagueContainer/LeagueContainer.js
@@ -0,0 +1,8 @@
+import data from "../../db/standingsMockData.json";
+import styles from "./LeagueContainer.module.scss";
+
+const LeagueContainer = () => {
+	return <h2 className={styles.title}>{data.league}</h2>;
+};
+
+export default LeagueContainer;
