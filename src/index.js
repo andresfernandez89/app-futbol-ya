3fbea79059282e826f30b3bfb7c0aa3cec9c068d
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2022-09-19 18:28:48 -0300

    react-router-dom added on index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
 import App from "./components/app/App";
 import "antd/dist/antd.min.css";
 import "./index.scss";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
