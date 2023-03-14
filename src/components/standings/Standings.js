6e1b650fa04396728ffde3f0457c0a6f7b10164f
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2023-03-13 23:00:04 -0300

    real mockData aplied on component

--- src/components/standings/Standings.js
+++ src/components/standings/Standings.js
@@ -1,67 +1,93 @@
 import { Table } from "antd";
-import dataStanding from "../../db/standingsMockData.json";
+import { useEffect, useState } from "react";
 import styles from "./Standings.module.scss";
+import data from "../../db/getBystandingsArg1.json";
 
 const columns = [
-  {
-    title: "Rank",
-    dataIndex: "rank",
-  },
-  {
-    title: "",
-    dataIndex: "logo",
-    render: (logo) => (
-      <img className={styles.logo} src={logo} alt="logo del club"></img>
-    ),
-  },
-  {
-    title: "Club",
-    dataIndex: "club",
-    render: (club) => <a href="index.html">{club}</a>,
-  },
-  {
-    title: "Pts",
-    dataIndex: "points",
-  },
-  {
-    title: "PJ",
-    dataIndex: "pj",
-  },
-  {
-    title: "PG",
-    dataIndex: "pg",
-  },
-  {
-    title: "PE",
-    dataIndex: "pe",
-  },
-  {
-    title: "PP",
-    dataIndex: "pp",
-  },
-  {
-    title: "GF",
-    dataIndex: "gf",
-  },
-  {
-    title: "GC",
-    dataIndex: "gc",
-  },
-  {
-    title: "DIF",
-    dataIndex: "dif",
-  },
+	{
+		title: "Rank",
+		dataIndex: "rank",
+	},
+	{
+		title: "",
+		dataIndex: "logo",
+		render: (logo) => <img className={styles.logo} src={logo} alt="logo del club"></img>,
+	},
+	{
+		title: "Club",
+		dataIndex: "name",
+		render: (club) => <a href="index.html">{club}</a>,
+	},
+	{
+		title: "Pts",
+		dataIndex: "points",
+	},
+	{
+		title: "PJ",
+		dataIndex: "played",
+	},
+	{
+		title: "PG",
+		dataIndex: "win",
+	},
+	{
+		title: "PE",
+		dataIndex: "draw",
+	},
+	{
+		title: "PP",
+		dataIndex: "lose",
+	},
+	{
+		title: "GF",
+		dataIndex: "gf",
+	},
+	{
+		title: "GC",
+		dataIndex: "ga",
+	},
+	{
+		title: "DIF",
+		dataIndex: "goalsDiff",
+	},
 ];
 
-const Standings = () => (
-  <div className={styles.standingsContainer}>
-    <h4 className={styles.title}>TABLA DE POSICIONES</h4>
-    <Table
-      columns={columns}
-      dataSource={dataStanding.standings}
-      size="small"
-      pagination={false}
-    />
-  </div>
-);
+const Standings = ({ dataLeague }) => {
+	const [id, setId] = useState(dataLeague.id);
+	const [season, setSeason] = useState(dataLeague.season);
+	const [dataStandings, setDataStandings] = useState(null);
+
+	useEffect(() => {
+		const newObj = data.response[0].league.standings[1].map((obj) => {
+			return {
+				key: obj.team.id,
+				rank: obj.rank,
+				logo: obj.team.logo,
+				name: obj.team.name,
+				points: obj.points,
+				played: obj.all.played,
+				win: obj.all.win,
+				draw: obj.all.draw,
+				lose: obj.all.lose,
+				gf: obj.all.goals.for,
+				ga: obj.all.goals.against,
+				goalsDiff: obj.goalsDiff,
+			};
+		});
+
+		setTimeout(setDataStandings(newObj), 2000);
+	}, [id, season]);
+	return (
+		<div className={styles.standingsContainer}>
+			{dataStandings ? (
+				<>
+					<h4 className={styles.title}>TABLA DE POSICIONES</h4>
+					<Table columns={columns} dataSource={dataStandings} size="small" pagination={false} />
+				</>
+			) : (
+				""
+			)}
+		</div>
+	);
+};
 export default Standings;
