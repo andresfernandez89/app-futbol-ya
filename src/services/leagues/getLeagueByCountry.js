1a2e1284742cc15e331ef5314cdc58cf41ac88d9
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2023-03-21 19:34:43 -0300

    files deleted

--- src/services/leagues/getLeagueByCountry.js
+++
@@ -1,7 +0,0 @@
-import {getApiUrl} from "../../config";
-
-export const getLeagueByCountry = async (country) => {
-	return await getApiUrl(
-		`leagues?season=${new Date().getFullYear()}&country=${country}&type=league`
-	);
-};
