b73c65c4247a13b6af54167b555aec42f3f5ce90
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2023-03-21 19:38:08 -0300

    DefaulContentLeague --> DefaultContentLeague and StrictMode actived

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import "antd/dist/antd.min.css";
 import "./index.scss";
 import RootLayout from "./pages/RootLayout/RootLayout";
 import DetailContainer from "./components/detailContainer/DetailContainer";
-import DefaulContentLeague from "./components/defaultContentLeague/DefaultContentLeague";
+import DefaultContentLeague from "./components/defaultContentLeague/DefaultContentLeague";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<RootLayout />} errorElement={<NotFound />}>
 			<Route element={<DetailContainer />}>
-				<Route index element={<DefaulContentLeague />} />
+				<Route index element={<DefaultContentLeague />} />
 				<Route path="posiciones" element={<h3>Posiciones</h3>} />
 				<Route path="calendario" element={<h3>Calendario</h3>} />
 				<Route path="goleadores" element={<h3>Goleadores</h3>} />
@@ -27,8 +27,9 @@ const router = createBrowserRouter(
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-{
-	/* <React.StrictMode>
-</React.StrictMode> */
-}
-root.render(<RouterProvider router={router} />);
+
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
