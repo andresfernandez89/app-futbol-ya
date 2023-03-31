bc8371b347e80c4b0d2905e1105cd839bcb8b36d
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2023-03-31 08:15:55 -0300

    destructuring and optional chaining added

--- src/components/fixture/Fixture.js
+++ src/components/fixture/Fixture.js
@@ -16,74 +16,72 @@ const Fixture = ({ dataLeague }) => {
 	return (
 		<>
 			<h4 className={styles.title}>FIXTURE</h4>
-			{dataFixture
-				? dataFixture.map((data) => {
-						let statusMatch;
-						if (data.fixture.status.short === "FT") {
-							statusMatch = "Finalizado";
-						} else if (data.fixture.status.short === "1H") {
-							statusMatch = "Primer Tiempo";
-						} else if (data.fixture.status.short === "HT") {
-							statusMatch = "Entretiempo";
-						} else if (data.fixture.status.short === "2H") {
-							statusMatch = "Segundo Tiempo";
-						} else if (data.fixture.status.short === "TBD") {
-							const date = new Date(data.fixture.timestamp * 1000);
-							const day = date.getDate();
-							const month = date
-								.toLocaleString("es-ES", { month: "long" })
-								.replace(/^\w/, (c) => c.toUpperCase());
-							const dayOfWeek = date
-								.toLocaleString("es-ES", { weekday: "long" })
-								.replace(/^\w/, (c) => c.toUpperCase());
-							const hour = date.getHours();
-							let minutes = date.getMinutes().toString().padEnd(2, "0");
-							statusMatch = `${dayOfWeek} ${day} de ${month} ${hour}:${minutes} Hs`;
-						}
-						return (
-							<div key={data.fixture.id} className={styles.container}>
-								<Row justify="center" align="middle">
-									<Col className={styles.status}>{statusMatch}</Col>
-								</Row>
-								<Row justify="center" wrap={false}>
-									<Col xs={24} sm={24} md={24}>
-										<Row className={styles.match} justify="center" wrap={false} align="middle">
-											<Col
-												xs={11}
-												sm={11}
-												md={10}
-												lg={10}
-												className={`${styles.containerTeam} ${styles.containerTeamA}`}
-											>
-												<span className={styles.team}>{data.teams.home.name}</span>
-												<img src={data.teams.home.logo} className={styles.logo} alt="logo club" />
-												<span className={styles.result}>
-													{data.score.extratime.home || data.score.fulltime.home}
-												</span>
-											</Col>
-											<Col xs={2} sm={2} className={styles.versus}>
-												vs
-											</Col>
-											<Col
-												xs={11}
-												sm={11}
-												md={10}
-												lg={10}
-												className={`${styles.containerTeam} ${styles.containerTeamB}`}
-											>
-												<span className={styles.result}>
-													{data.score.extratime.away || data.score.fulltime.away}
-												</span>
-												<img src={data.teams.away.logo} className={styles.logo} alt="logo club" />
-												<span className={styles.team}>{data.teams.away.name}</span>
-											</Col>
-										</Row>
+			{dataFixture?.map(({ fixture, teams, score }) => {
+				let statusMatch;
+				if (fixture.status.short === "FT") {
+					statusMatch = "Finalizado";
+				} else if (fixture.status.short === "1H") {
+					statusMatch = "Primer Tiempo";
+				} else if (fixture.status.short === "HT") {
+					statusMatch = "Entretiempo";
+				} else if (fixture.status.short === "2H") {
+					statusMatch = "Segundo Tiempo";
+				} else if (fixture.status.short === "TBD") {
+					const date = new Date(fixture.timestamp * 1000);
+					const day = date.getDate();
+					const month = date
+						.toLocaleString("es-ES", { month: "long" })
+						.replace(/^\w/, (c) => c.toUpperCase());
+					const dayOfWeek = date
+						.toLocaleString("es-ES", { weekday: "long" })
+						.replace(/^\w/, (c) => c.toUpperCase());
+					const hour = date.getHours();
+					let minutes = date.getMinutes().toString().padEnd(2, "0");
+					statusMatch = `${dayOfWeek} ${day} de ${month} ${hour}:${minutes} Hs`;
+				}
+				return (
+					<div key={fixture.id} className={styles.container}>
+						<Row justify="center" align="middle">
+							<Col className={styles.status}>{statusMatch}</Col>
+						</Row>
+						<Row justify="center" wrap={false}>
+							<Col xs={24} sm={24} md={24}>
+								<Row className={styles.match} justify="center" wrap={false} align="middle">
+									<Col
+										xs={11}
+										sm={11}
+										md={10}
+										lg={10}
+										className={`${styles.containerTeam} ${styles.containerTeamA}`}
+									>
+										<span className={styles.team}>{teams.home.name}</span>
+										<img src={teams.home.logo} className={styles.logo} alt="logo club" />
+										<span className={styles.result}>
+											{score.extratime.home || score.fulltime.home}
+										</span>
+									</Col>
+									<Col xs={2} sm={2} className={styles.versus}>
+										vs
+									</Col>
+									<Col
+										xs={11}
+										sm={11}
+										md={10}
+										lg={10}
+										className={`${styles.containerTeam} ${styles.containerTeamB}`}
+									>
+										<span className={styles.result}>
+											{score.extratime.away || score.fulltime.away}
+										</span>
+										<img src={teams.away.logo} className={styles.logo} alt="logo club" />
+										<span className={styles.team}>{teams.away.name}</span>
 									</Col>
 								</Row>
-							</div>
-						);
-				  })
-				: ""}
+							</Col>
+						</Row>
+					</div>
+				);
+			})}
 		</>
 	);
 };
