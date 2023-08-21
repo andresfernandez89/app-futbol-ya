495865203c9779b063b51206f14e31670aa39a2f
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2023-08-21 20:06:53 -0300

    style: new style in LeagueContainer Component

--- src/components/leagueContainer/LeagueContainer.jsx
+++ src/components/leagueContainer/LeagueContainer.jsx
@@ -2,7 +2,7 @@ import data from "../../db/standingsMockData.json";
 import styles from "./LeagueContainer.module.scss";
 
 const LeagueContainer = () => {
-  return <h2 className={styles.title}>{data.league}</h2>;
+	return <h2 className={styles.title}>{data.league}</h2>;
 };
 
 export default LeagueContainer;
